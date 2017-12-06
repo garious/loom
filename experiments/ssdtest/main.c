a04c57a610661098fef6bb33b51ea95e1130df5c
Anatoly Yakovenko <aeyakovenko@gmail.com>
2017-12-06 10:12:29 -0800

    update

--- experiments/ssdtest/main.c
+++ experiments/ssdtest/main.c
@@ -17,8 +17,10 @@
 //#define SHOW_DEBUG
 #include "err.h"
 
-#define TSIZE (512*1024*1024*1024llu)
+#define TSIZE (640*1024*1024*1024llu)
 #define QSIZE (512*1024)
+#define BUFSIZE 32
+
 struct tx {
     off_t from;
     off_t to;
@@ -27,34 +29,24 @@ struct tx {
 static struct tx queue[QSIZE];
 uint64_t rix;
 
-#define OPSIZE (2*QSIZE)
-static uint32_t ops[OPSIZE];
 
 void *run_thread(void *ctx) {
     int *count = (int*)ctx;
 	int err = 0;
     int fd = 0;
-    uint8_t buf[64];
+    uint8_t buf[BUFSIZE];
 	TEST(err, 0 != (fd = open("table", O_RDWR | O_NOATIME, 0)));
     while(1) {
         uint64_t ix = __sync_fetch_and_add(&rix, 1);
-        uint32_t op = ops[ix % OPSIZE];
-        uint32_t tix = op;
-        uint32_t flag = 0;
-        if(op >= QSIZE) {
-            flag = 1;
-            tix = op - QSIZE;
-        } 
-        struct tx *tx = &queue[tix];
-        if(!flag) {
-            TEST(err, (off_t)-1 != lseek(fd, tx->from, SEEK_SET));
-        } else {
-            TEST(err, (off_t)-1 != lseek(fd, tx->to, SEEK_SET));
-        }
-        TEST(err, 64 == read(fd, buf, 64));
-        *count = *count + 1;
+        struct tx *tx = &queue[ix % QSIZE];
+        TEST(err, (off_t)-1 != lseek(fd, tx->from, SEEK_SET));
+        TEST(err, BUFSIZE == read(fd, buf, BUFSIZE));
+        TEST(err, (off_t)-1 != lseek(fd, tx->to, SEEK_SET));
+        TEST(err, BUFSIZE == read(fd, buf, BUFSIZE));
+        *count = *count + 2;
     }
 CHECK(err):
+    assert(0);
     return 0;
 }
 
@@ -82,15 +74,10 @@ int main(int argc, const char *argv[]) {
 	int64_t size = TSIZE;
     double total;
     int threads = atoi(argv[1]);
-    for(i = 0; i < OPSIZE; i++) {
-        ops[i] = i;
-    }
     for(i = 0; i < QSIZE; i++) {
-        queue[i].from = MAX(0, (((double)rand())/(double)RAND_MAX * (double)size) - 64);
-        queue[i].to = MAX(0, (((double)rand())/(double)RAND_MAX * (double)size) - 64);
+        queue[i].from = ((off_t)(MAX(0, (((double)rand())/(double)RAND_MAX * (double)size)))) & (~(32 - 1));
+        queue[i].to = ((off_t)(MAX(0, (((double)rand())/(double)RAND_MAX * (double)size)))) & (~(32 - 1));
     }
-    qsort(&ops, OPSIZE, sizeof(ops[0]), compare);
-
 
     TEST(err, !gettimeofday(&start, 0));
     for(i = 0; i < threads; i++) {
@@ -107,10 +94,9 @@ int main(int argc, const char *argv[]) {
         prev = rix;
     	printf("speed %lu %ld %G %G tps\n", rix, diff, total, ((double)diff)/total * 1000000);
         for(i = 0; i < QSIZE; i++) {
-            queue[i].from = MAX(0, (((double)rand())/(double)RAND_MAX * (double)size) - 64);
-            queue[i].to = MAX(0, (((double)rand())/(double)RAND_MAX * (double)size) - 64);
+            queue[i].from = ((off_t)(MAX(0, (((double)rand())/(double)RAND_MAX * (double)size)))) & (~(32 - 1));
+            queue[i].to = ((off_t)(MAX(0, (((double)rand())/(double)RAND_MAX * (double)size)))) & (~(32 - 1));
         }
-        //qsort(&ops, OPSIZE, sizeof(ops[0]), compare);
     }
 CHECK(err):
     return err;
