d89de5960cbd3eae1595289684f74aef780c02fe
Anatoly Yakovenko <aeyakovenko@gmail.com>
2017-12-05 21:00:53 -0800

    update

--- inc/config.h
+++ inc/config.h
@@ -5,6 +5,7 @@
 #include "visibility.h"
 
 #define LOOM_PORT "10101"
+#define TSIZE (512*1024*1024*1024llu)
 
 PRIVATE const char* config_get(const char *env);
 PRIVATE int config_expand(const char *var, char **po);
