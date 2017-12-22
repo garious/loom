692e209e7186a1c60db5412ffd56ed5c38285710
Anatoly Yakovenko <aeyakovenko@gmail.com>
2017-12-22 14:54:52 -0800

    update

--- src/state.rs
+++ src/state.rs
@@ -1,5 +1,8 @@
 use data;
 use core::intrinsics::{atomic_xadd, atomic_xsub};
+use std::io::Result;
+use std::io::Error;
+use std::io::ErrorKind;
 
 #[repr(C)]
 struct Account {
@@ -13,53 +16,46 @@ pub struct State {
 }
 
 impl State {
-    fn new(size: usize) -> State {
+    pub fn new(size: usize) -> State {
         return State{accounts: Vec::with_capacity(size)};
     }
     fn key_to_hash(key: [u8; 32]) -> u64 {
-        return key[0] as u64<<((7 - 0) * 8) |
-               key[1] as u64<<((7 - 1) * 8) |
-               key[2] as u64<<((7 - 2) * 8) |
-               key[3] as u64<<((7 - 3) * 8) |
-               key[4] as u64<<((7 - 4) * 8) |
-               key[5] as u64<<((7 - 5) * 8) |
-               key[6] as u64<<((7 - 6) * 8) |
-               key[7] as u64<<((7 - 7) * 8) ;
+        return ((key[0] as u64) << ((7 - 0) * 8)) |
+               ((key[1] as u64) << ((7 - 1) * 8)) |
+               ((key[2] as u64) << ((7 - 2) * 8)) |
+               ((key[3] as u64) << ((7 - 3) * 8)) |
+               ((key[4] as u64) << ((7 - 4) * 8)) |
+               ((key[5] as u64) << ((7 - 5) * 8)) |
+               ((key[6] as u64) << ((7 - 6) * 8)) |
+               ((key[7] as u64) << ((7 - 7) * 8)) ;
     }
-    fn lookup(&mut self, key: [u8; 32]) -> &mut Account {
+    fn lookup(&mut self, key: [u8; 32]) -> Result<&mut Account> {
         let cap = self.accounts.capacity();
         let hash = State::key_to_hash(key) as usize;
         let ix = hash % cap;
-        for a in self.accounts[ix .. ].iter_mut() {
-            let zz = [0u8: 32];
-            if a.from == key {
-                return &mut a;
+        let x_ptr = self.accounts.as_mut_ptr();
+        unsafe {
+            for i in ix .. cap {
+                let a = x_ptr.offset(i as isize);
+                if (*a).from == key {
+                    return Ok(&mut (*a));
+                }
+                if (*a).from == [0u8;32] {
+                    return Ok(&mut (*a));
+                }
             }
-            if a.from == zz {
-                return &mut a;
-            }
+            return Err(Error::new(ErrorKind::Other, "oh no!"));
         }
-        for a in self.accounts[0 .. ix].iter_mut() {
-            let zz = [0u8: 32];
-            if a.from == key {
-                return &mut a;
-            }
-            if a.from == zz {
-                return &mut a;
-            }
-        }
-        //TODO(aey): handle no space
-        assert!(false);
-        return &mut self.accounts[0];
     }
-    fn withdrawals(&mut self, msgs: &mut [data::Message]) {
+
+    pub fn withdrawals(&mut self, msgs: &mut [data::Message]) -> Result<()> {
         for m in msgs {
             if m.kind != data::Kind::Transaction {
                 continue;
             }
             //TODO(aey) multiple threads
             unsafe {
-                let acc = self.lookup(m.data.tx.from);
+                let acc = self.lookup(m.data.tx.from)?;
                 let combined = m.data.tx.amount + m.data.tx.fee;
                 if acc.balance > combined {
                     m.state = data::State::Withdrawn;
@@ -68,22 +64,27 @@ impl State {
                 }
             }
         }
+        return Ok(());
     }
-    fn deposits(&mut self, msgs: &mut [data::Message]) {
+    pub fn deposits(&mut self, msgs: &mut [data::Message]) -> Result<()> {
         for m in msgs {
             if m.kind != data::Kind::Transaction {
                 continue;
             }
             if m.state == data::State::Withdrawn {
                 unsafe {
-                    let acc = self.lookup(m.data.tx.to);
+                    let acc = self.lookup(m.data.tx.to)?;
                     //TODO(aey) multiple threads
                     atomic_xadd((&mut acc.balance) as *mut u64,
                                 m.data.tx.amount);
+                    if acc.from == [0u8;32] {
+                        acc.from = m.data.tx.to;
+                    }
                 }
                 m.state = data::State::Deposited;
             }
         }
+        return Ok(());
     }
 }
 
@@ -91,7 +92,7 @@ impl State {
 fn state_test() {
     let mut s: State = State::new(64);
     let mut msgs = [];
-    s.withdrawals(&mut msgs);
-    s.deposits(&mut msgs);
+    s.withdrawals(&mut msgs).expect("withdrawals");
+    s.deposits(&mut msgs).expect("deposits");
 }
 
