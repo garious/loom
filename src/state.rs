d2563e5565301b925064ebdf29df15070469f85e
Anatoly Yakovenko <aeyakovenko@gmail.com>
2017-12-22 14:00:16 -0800

    update

--- src/state.rs
+++ src/state.rs
@@ -16,7 +16,40 @@ impl State {
     fn new(size: usize) -> State {
         return State{accounts: Vec::with_capacity(size)};
     }
+    fn key_to_hash(key: [u8; 32]) -> u64 {
+        return key[0] as u64<<((7 - 0) * 8) |
+               key[1] as u64<<((7 - 1) * 8) |
+               key[2] as u64<<((7 - 2) * 8) |
+               key[3] as u64<<((7 - 3) * 8) |
+               key[4] as u64<<((7 - 4) * 8) |
+               key[5] as u64<<((7 - 5) * 8) |
+               key[6] as u64<<((7 - 6) * 8) |
+               key[7] as u64<<((7 - 7) * 8) ;
+    }
     fn lookup(&mut self, key: [u8; 32]) -> &mut Account {
+        let cap = self.accounts.capacity();
+        let hash = State::key_to_hash(key) as usize;
+        let ix = hash % cap;
+        for a in self.accounts[ix .. ].iter_mut() {
+            let zz = [0u8: 32];
+            if a.from == key {
+                return &mut a;
+            }
+            if a.from == zz {
+                return &mut a;
+            }
+        }
+        for a in self.accounts[0 .. ix].iter_mut() {
+            let zz = [0u8: 32];
+            if a.from == key {
+                return &mut a;
+            }
+            if a.from == zz {
+                return &mut a;
+            }
+        }
+        //TODO(aey): handle no space
+        assert!(false);
         return &mut self.accounts[0];
     }
     fn withdrawals(&mut self, msgs: &mut [data::Message]) {
