3e000567143c78868a4440740af9366261c0d71d
Anatoly Yakovenko <aeyakovenko@gmail.com>
2017-12-24 12:53:14 -0800

    udpate

--- src/result.rs
+++ src/result.rs
@@ -1,15 +1,16 @@
 use std;
+use core;
 
-enum Error {
+pub enum Error {
     IO(std::io::Error),
     NoSpace,
     ToLarge,
 }
 
-type Result<T> = Result<T, Error>;
+pub type Result<T> = core::result::Result<T, Error>;
 
 pub fn fromIO<T>(r: std::io::Result<T>) -> Result<T> {
-    match {
+    match r {
         Ok(t) => Ok(t),
         Err(e) => Err(Error::IO(e)),
     }
