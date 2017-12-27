b28cfec9d4c80aee7ae200fd0cb3efcb70b8da0e
Anatoly Yakovenko <aeyakovenko@gmail.com>
2017-12-26 19:08:34 -0800

    update

--- src/spool.rs
+++ src/spool.rs
@@ -1,18 +1,19 @@
 use net;
 use state;
-use std::io::Result;
+use data;
+use result::Result;
+use std::mem::uninitialized;
 
 pub fn run() -> Result<()> {
     let srv = net::server()?;
-    let s = state::new();
-    let mut m: [Message; 1024] = unsafe { uninitialized() };
+    let mut s = state::State::new(1024);
+    let mut m: [data::Message; 1024] = unsafe { uninitialized() };
     let mut num = 0;
     loop {
         //TODO(aey): read/execute on separate threads
         let start = num;
-        net::read(srv, &mut m[start .. ], &mut num).expect("read");
+        net::read(&srv, &mut m[start .. ], &mut num).expect("read");
         let end = num;
         s.execute(&mut m[start .. end]).expect("execute");
     }
-    return Ok(());
 }
