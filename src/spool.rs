76e3c4b0fd6063af3e7a476090f1942a6fba6b5a
Anatoly Yakovenko <aeyakovenko@gmail.com>
2017-12-26 18:33:23 -0800

    update

--- src/spool.rs
+++ src/spool.rs
@@ -1,23 +1,18 @@
 use net;
-use poh;
+use state;
 use std::io::Result;
 
 pub fn run() -> Result<()> {
     let srv = net::server()?;
+    let s = state::new();
     let mut m: [Message; 1024] = unsafe { uninitialized() };
     let mut num = 0;
     loop {
+        //TODO(aey): read/execute on separate threads
         let start = num;
         net::read(srv, &mut m[start .. ], &mut num).expect("read");
         let end = num;
-        poh::verify(&mut m[start .. end], &mut num).expect("verify");
-        execute(&m[start .. num], &mut num).expect("execute");
+        s.execute(&mut m[start .. end]).expect("execute");
     }
     return Ok(());
-}
-
-pub fn execute(msgs: &[Message]) -> Result<()> {
-    let mut order = [0usize; 1024];
-    sort(msgs, &mut order);
-    apply(&order, msgs);
 }
