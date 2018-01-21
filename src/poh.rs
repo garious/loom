ff0ba4cf2cfbf79f957581f581166039f10c02b0
Anatoly Yakovenko <aeyakovenko@gmail.com>
2018-01-20 17:25:07 -0800

    update

--- src/poh.rs
+++ src/poh.rs
@@ -1,6 +1,25 @@
-use data::{Message};
-use std::io::Result;
+use data;
+use hasht;
+use result;
 
-pub fn verify(m: &[Message], num: &mut usize) -> Result<()> {
-    return Ok(());
+struct Subscriber {
+    key: [u8; 32],
+    addr: SockAddr,
+    lastping: u64,
+}
+
+struct Gossip {
+    subscribers: Vec<Subscriber>,
+}
+
+impl Gossip {
+    pub fn execute(&self, msgs: &mut [data::Message]) {
+        for m in msgs.iter() {
+            if m.kind == GossipSubscribe {
+                let pos = hasht::find(&self.subscribers, m.data.subs.key)?;
+                let update = Subscriber{m.data.subs.addr, m.data.key, now};
+                *self.subscribers.get_unchecked_mut(pos) = update;
+            }
+        }
+    }
 }
