ce72451cfd00712fac5286add6eabd641ba2afc7
Anatoly Yakovenko <aeyakovenko@gmail.com>
2018-02-01 07:21:56 -0800

    reply

--- src/ledger.rs
+++ src/ledger.rs
@@ -2,6 +2,7 @@
 use std::fs::File;
 use std::io::{Seek, SeekFrom, Write, Read};
 use result::Result;
+use mio::net::UdpSocket;
 use std::mem::transmute;
 use std::mem::size_of;
 use std::slice::from_raw_parts;
@@ -20,22 +21,33 @@ impl Ledger {
         let l = Ledger{file: file};
         return Ok(l);
     }
-    fn get_ledger(&self, get: &data::GetLedger) -> Result<()> {
+    fn get_ledger(&self, sock: &UdpSocket,
+                  get: &data::GetLedger) -> Result<()> {
+        let mut mem = Vec::new();
+        mem.resize(get.num as usize, data::Message::default());
+        Self::load(&mut mem, get.start)?;
+        let p = &mem[0] as *const data::Message;
+        let sz = size_of::<data::Message>();
+        let bz = mem.len() * sz;
+        let buf = unsafe {
+            transmute(from_raw_parts(p as *const u8, bz))
+        };
+        sock.send(buf)?;
         return Ok(());
     }
-    fn exec(&self, m: &data::Message) -> Result<()> {
+    fn exec(&self, sock: &UdpSocket, m: &data::Message) -> Result<()> {
         match m.pld.kind {
             data::Kind::GetLedger => {
                 let get = unsafe {&m.pld.data.get};
-                self.get_ledger(get)?;
+                self.get_ledger(sock, get)?;
             }
             _ => return Ok(()),
         };
         return Ok(());
     }
-    pub fn execute(&self, msgs: &mut [data::Message]) -> Result<()> {
+    pub fn execute(&self, sock: &UdpSocket, msgs: &mut [data::Message]) -> Result<()> {
         for m in msgs.iter_mut() {
-            self.exec(&m)?;
+            self.exec(sock, &m)?;
         }
         return Ok(());
     }
@@ -52,10 +64,10 @@ impl Ledger {
         self.file.write_all(buf)?;
         return Ok(());
     }
-    pub fn read(msgs: &mut [data::Message], start: u64) -> Result<()> {
+    pub fn load(msgs: &mut [data::Message], start: u64) -> Result<()> {
         //TODO(aeyakovenko): the fastest way to do this:
         // have the msgs memory be mmaped
-        // then `splice` from mmap fd
+        // then `splice` from mmap fd, or to the socket directly
         let mut file = File::open(LEDGER)?;
         let p = &mut msgs[0] as *mut data::Message;
         let sz = size_of::<data::Message>();
